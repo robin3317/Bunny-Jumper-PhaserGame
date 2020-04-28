fa2ad993e4fa367cd5a166d7921d271923da44a0
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2020-04-28 21:49:52 +0600

    adding collisions

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -26,7 +26,11 @@ class Game extends Phaser.Scene {
       body.updateFromGameObject();
     }
 
-    this.physics.add.sprite(240, 320, 'bunny-stand').setScale(0.5);
+    const player = this.physics.add
+      .sprite(240, 320, 'bunny-stand')
+      .setScale(0.5);
+
+    this.physics.add.collider(platforms, player);
   }
 }
 
