dbffe5680854c63960fc87447081c173d321bb18
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2020-04-28 21:45:18 +0600

    creating the player

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -8,6 +8,7 @@ class Game extends Phaser.Scene {
   preload() {
     this.load.image('background', 'assets/bg_layer1.png');
     this.load.image('platform', 'assets/ground_grass.png');
+    this.load.image('bunny-stand', 'assets/bunny1_stand.png');
   }
 
   create() {
@@ -24,6 +25,8 @@ class Game extends Phaser.Scene {
       const body = platform.body;
       body.updateFromGameObject();
     }
+
+    this.physics.add.sprite(240, 320, 'bunny-stand').setScale(0.5);
   }
 }
 
